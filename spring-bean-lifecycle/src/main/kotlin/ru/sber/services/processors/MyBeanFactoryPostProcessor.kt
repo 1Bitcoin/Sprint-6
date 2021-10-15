ace1fbf3cb412fe62abae89fcad9143c556aa741
Дмитрий l W.W <shigalkin@mail.ru>
2021-10-15 17:19:19 +0300

    done bean lifecycle

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
@@ -1,11 +1,24 @@
 package ru.sber.services.processors
 
+import org.springframework.beans.BeansException
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+import org.springframework.beans.factory.support.DefaultListableBeanFactory
+import org.springframework.beans.factory.support.GenericBeanDefinition
 import org.springframework.stereotype.Component
+import ru.sber.services.BeanFactoryPostProcessorBean
+
 
 @Component
 class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor {
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
+
+        val bd = GenericBeanDefinition()
+        bd.setBeanClass(BeanFactoryPostProcessorBean::class.java)
+
+        bd.initMethodName = "postConstruct"
+
+        (beanFactory as DefaultListableBeanFactory).registerBeanDefinition("beanFactoryPostProcessorBean", bd)
     }
 }
+
